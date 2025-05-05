11ff28ee89c8c46e9d8f059ee5605012dc74cb0a
Arun <arunjamdagni2@gmail.com>
2025-05-05 06:57:11 +0530

    change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,36 +13,29 @@ const AboutUs = () => {
       <Banner heading="About Us" image={"/images/banner-contact.avif"} />
       <HowItWorks />
       <section
-        className="max-w-screen-xl relative h-[40vh]
-         bg-cover bg-center bg-no-repeat
-         bg-fixed"
-        style={{ backgroundImage: `url('/images/banner-main-1.jpg')` }}
-      >
-        <div className="absolute h-full w-full bg-black opacity-50"></div>
-        <div
-          className="w-full
-            absolute z-10
-             text-white
-            top-1/2 left-1/2
-             transform -translate-y-1/2
-              -translate-x-1/2 max-w-2xl"
-        >
-          <div className="max-w-2xl w-full flex items-center justify-between">
-            <article className="space-y-5 text-center">
-              <h1 className="text-white">540+ </h1>
-              <p className="text-white">Properties Sell</p>
-            </article>
-            <article className="space-y-5 text-center">
-              <h1 className="text-white">20+ </h1>
-              <p className="text-white">Award Gained</p>
-            </article>
-            <article className="space-y-5 text-center">
-              <h1 className="text-white">9+ </h1>
-              <p className="text-white">Years Experience</p>
-            </article>
-          </div>
-        </div>
-      </section>
+  className="relative w-full h-[40vh] bg-cover bg-center bg-no-repeat bg-fixed"
+  style={{ backgroundImage: `url('/images/banner-main-1.jpg')` }}
+>
+  <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
+
+  <div className="relative z-10 w-full h-full flex items-center justify-center text-white text-center">
+    <div className="flex space-x-60">
+      <article className="space-y-8">
+        <h1 className="text-3xl font-bold text-white">540+</h1>
+        <p className="text-white">Properties Sold</p>
+      </article>
+      <article className="space-y-8">
+        <h1 className="text-3xl font-bold text-white">20+</h1>
+        <p className="text-white">Awards Gained</p>
+      </article>
+      <article className="space-y-8">
+        <h1 className="text-3xl font-bold text-white">9+</h1>
+        <p className="text-white">Years Experience</p>
+      </article>
+    </div>
+  </div>
+</section>
+
       <Team />
       <Testimonials />
       <Footer />
